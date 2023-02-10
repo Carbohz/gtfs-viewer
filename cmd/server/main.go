8a67ca50a0e06fdc55a4dd8b254c41346761f9f7
Carbohz <Ezpath@yandex.ru>
2023-02-10 22:50:32 +0300

    add dependencies

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/MobilityData/gtfs-realtime-bindings/golang/gtfs"
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var (
+		username = "YOUR_ACCESS_KEY"
+		password = "YOUR_SECRET_KEY"
+	)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "URL OF YOUR GTFS-REALTIME SOURCE GOES HERE", nil)
+	req.SetBasicAuth(username, password)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	feed := gtfs.FeedMessage{}
+	err = proto.Unmarshal(body, &feed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entity := range feed.Entity {
+		tripUpdate := entity.GetTripUpdate()
+		trip := tripUpdate.GetTrip()
+		tripId := trip.GetTripId()
+		fmt.Printf("Trip ID: %s\n", tripId)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Carbohz/gtfs-viewer
 
 go 1.17
+
+require (
+	github.com/MobilityData/gtfs-realtime-bindings/golang/gtfs v1.0.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
